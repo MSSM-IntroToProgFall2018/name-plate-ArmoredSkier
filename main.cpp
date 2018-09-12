fe80f3db7247e8d79b7cfdd7932fdcaafcf367fe
ArmoredSkier <goodwinj@mssm.org>
2018-09-12 11:49:26 -0400

    First Commit

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string name;
+    int size;
+    cout << "What is your first name?" << endl;
+    cin >> name;
+    size = name.length();
+
+    if (size > 15) {
+
+        cout << "Your name is too long for this program." << endl;
+
+    }
+    else {
+
+        for (int i = 0; i < size+4; i++) {
+            cout << "*";
+        }
+
+        cout << "\n";
+        cout << "*";
+
+        for (int i = 0; i < size+2; i++) {
+            cout << " ";
+        }
+
+        cout << "*";
+        cout << "\n";
+        cout << "* ";
+        cout << name;
+        cout << " *";
+        cout << "\n";
+        cout << "*";
+
+        for (int i = 0; i < size+2; i++) {
+            cout << " ";
+        }
+
+        cout << "*";
+        cout << "\n";
+
+        for (int i = 0; i < size+4; i++) {
+            cout << "*";
+        }
+        cout << "\n";
+
+    }
+
+}
